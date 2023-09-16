5afd5521c172b7d0a4c04eae160649a30a21fb7d
Duda <98721183+MariaEduardaBernardo@users.noreply.github.com>
2023-09-15 22:20:06 -0300

    Melhorias no tratamento de erros PHP

--- dados.php
+++ dados.php
@@ -4,46 +4,72 @@ $username = "root";
 $password = "";
 $dbname = "estudante1";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+try {
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Erro na conexão com o banco de dados: " . $conn->connect_error);
-}
-
-$genero = isset($_POST['genero']) ? mysqli_real_escape_string($conn, $_POST['genero']) : 'todos';
-$tipo_diabetes = isset($_POST['tipo_diabetes']) ? mysqli_real_escape_string($conn, $_POST['tipo_diabetes']) : 'todos';
-$sintomas = isset($_POST['sintomas']) ? $_POST['sintomas'] : [];
-
-$sql = "SELECT * FROM tabela_de_dados WHERE 1 = 1";
-
-if ($genero != 'todos') {
-    $sql .= " AND genero = '$genero'";
-}
-
-if ($tipo_diabetes != 'todos') {
-    $sql .= " AND tipo_diabetes = '$tipo_diabetes'";
-}
-
-if (!empty($sintomas)) {
-    $sintomasCondicao = implode("','", $sintomas);
-    $sql .= " AND sintoma IN ('$sintomasCondicao')";
-}
-
-$result = $conn->query($sql);
-
-// Exibir os resultados no HTML
-if ($result->num_rows > 0) {
-    echo "<table>";
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr>";
-        echo "<td>" . $row['campo1'] . "</td>";
-        echo "<td>" . $row['campo2'] . "</td>";
-        echo "</tr>";
+    if ($conn->connect_error) {
+        throw new Exception("Erro na conexão com o banco de dados: " . $conn->connect_error);
     }
-    echo "</table>";
-} else {
-    echo "Nenhum resultado encontrado.";
-}
 
-$conn->close();
+    $genero = isset($_POST['genero']) ? mysqli_real_escape_string($conn, $_POST['genero']) : 'todos';
+    $tipo_diabetes = isset($_POST['tipo_diabetes']) ? mysqli_real_escape_string($conn, $_POST['tipo_diabetes']) : 'todos';
+    $sintomas = isset($_POST['sintomas']) ? $_POST['sintomas'] : [];
+
+    $sql = "SELECT campo1, campo2 FROM pacientes WHERE 1 = 1";
+
+    if ($genero != 'todos') {
+        $sql .= " AND genero = ?";
+    }
+
+    if ($tipo_diabetes != 'todos') {
+        $sql .= " AND tipo_diabetes = ?";
+    }
+
+    if (!empty($sintomas)) {
+        $placeholders = implode(', ', array_fill(0, count($sintomas), '?'));
+        $sql .= " AND sintoma IN ($placeholders)";
+    }
+
+    $stmt = $conn->prepare($sql);
+
+    if (!$stmt) {
+        throw new Exception("Erro na preparação da consulta: " . $conn->error);
+    }
+
+    if ($genero != 'todos') {
+        $stmt->bind_param("s", $genero);
+    }
+
+    if ($tipo_diabetes != 'todos') {
+        $stmt->bind_param("s", $tipo_diabetes);
+    }
+
+    if (!empty($sintomas)) {
+        $stmt->bind_param(str_repeat("s", count($sintomas)), ...$sintomas);
+    }
+
+    if (!$stmt->execute()) {
+        throw new Exception("Erro na execução da consulta: " . $stmt->error);
+    }
+
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        echo "<table>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row['campo1'] . "</td>";
+            echo "<td>" . $row['campo2'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "Nenhum resultado encontrado.";
+    }
+
+    $stmt->close();
+    $conn->close();
+} catch (Exception $e) {
+    echo "Erro: " . $e->getMessage();
+}
 ?>
